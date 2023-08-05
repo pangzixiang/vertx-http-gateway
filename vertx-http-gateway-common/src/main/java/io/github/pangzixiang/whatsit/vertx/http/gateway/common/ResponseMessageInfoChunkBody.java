90a3ac124c1c776fe8f866d16c47ebf4d5b2f2f5
pangzixiang <313227220@qq.com>
2023-08-05 15:22:18 +0800

    support proxy websocket

--- vertx-http-gateway-common/src/main/java/io/github/pangzixiang/whatsit/vertx/http/gateway/common/ResponseMessageInfoChunkBody.java
+++ vertx-http-gateway-common/src/main/java/io/github/pangzixiang/whatsit/vertx/http/gateway/common/ResponseMessageInfoChunkBody.java
@@ -1,25 +1,28 @@
 package io.github.pangzixiang.whatsit.vertx.http.gateway.common;
 
 import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpVersion;
 import lombok.Getter;
 
 import java.util.Arrays;
 
 @Getter
 public class ResponseMessageInfoChunkBody {
+    private final HttpVersion httpVersion;
     private final String statusMessage;
     private final int statusCode;
     private final MultiMap headers = MultiMap.caseInsensitiveMultiMap();
 
     /**
-     * 200 OK
+     * http/1.1 200 OK
      * [headers]
      */
     public ResponseMessageInfoChunkBody(String responseInfoChunkBody) {
         String[] chunkLines = responseInfoChunkBody.split(System.lineSeparator());
         String[] firstLine = chunkLines[0].split(" ");
-        this.statusMessage = String.join(" ", Arrays.asList(firstLine).subList(1, firstLine.length));
-        this.statusCode = Integer.parseInt(firstLine[0]);
+        this.httpVersion = Utils.httpVersionParser(firstLine[0]);
+        this.statusMessage = String.join(" ", Arrays.asList(firstLine).subList(2, firstLine.length));
+        this.statusCode = Integer.parseInt(firstLine[1]);
         for (int i = 1; i < chunkLines.length; i++) {
             String[] line = chunkLines[i].split(":");
             this.headers.add(line[0], line[1]);
@@ -27,11 +30,11 @@ public class ResponseMessageInfoChunkBody {
     }
 
     /**
-     * 200 OK
+     * http/1.1 200 OK
      * [headers]
      */
-    public static String build(String statusMessage, int statusCode, MultiMap headers) {
-        String firstLine = statusCode + " " + statusMessage + System.lineSeparator();
+    public static String build(HttpVersion httpVersion, String statusMessage, int statusCode, MultiMap headers) {
+        String firstLine = httpVersion.alpnName() + " " + statusCode + " " + statusMessage + System.lineSeparator();
         StringBuilder result = new StringBuilder(firstLine);
         headers.forEach((key, value) -> {
             result.append(key).append(":").append(value).append(System.lineSeparator());
