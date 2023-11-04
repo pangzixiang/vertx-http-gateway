729d23aee5d0011b9fd6ed08307c87a28bd2105e
pangzixiang <65987771+pangzixiang@users.noreply.github.com>
2023-11-04 23:00:00 +0800

    long connection for ws (#30)

--- vertx-http-gateway-connector/src/main/java/io/github/pangzixiang/whatsit/vertx/http/gateway/connector/VertxHttpGatewayConnectorMainVerticle.java
+++ vertx-http-gateway-connector/src/main/java/io/github/pangzixiang/whatsit/vertx/http/gateway/connector/VertxHttpGatewayConnectorMainVerticle.java
@@ -4,15 +4,22 @@ import io.github.pangzixiang.whatsit.vertx.http.gateway.connector.handler.EventH
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.*;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@AllArgsConstructor
 class VertxHttpGatewayConnectorMainVerticle extends AbstractVerticle {
     private final VertxHttpGatewayConnectorOptions vertxHttpGatewayConnectorOptions;
     private final EventHandler eventHandler;
+
+    private Long waitPongTask;
+
+    public VertxHttpGatewayConnectorMainVerticle(VertxHttpGatewayConnectorOptions vertxHttpGatewayConnectorOptions, EventHandler eventHandler) {
+        this.vertxHttpGatewayConnectorOptions = vertxHttpGatewayConnectorOptions;
+        this.eventHandler = eventHandler;
+    }
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
@@ -47,7 +54,27 @@ class VertxHttpGatewayConnectorMainVerticle extends AbstractVerticle {
                             log.debug("Succeeded to register to vertx http gateway [{}:{}{}]!", options.getHost(), options.getPort(), options.getURI());
                             promise.complete();
                             eventHandler.afterEstablishConnection(ws);
+
+                            ws.pongHandler(buffer -> {
+                                log.trace("Received pong from server");
+                                if (waitPongTask != null) {
+                                    getVertx().cancelTimer(waitPongTask);
+                                }
+                            });
+
+                            long pingTaskId = getVertx().setPeriodic(0, 5000, l -> {
+                                log.trace("Send ping to server");
+                                waitPongTask = getVertx().setTimer(3000, l2 -> {
+                                    log.error("Failed to get pong from server, will reconnect...");
+                                    ws.close();
+                                });
+                                ws.writePing(Buffer.buffer("ping")).onFailure(throwable -> {
+                                    log.error("Failed to send ping to server", throwable);
+                                    ws.close();
+                                });
+                            });
                             ws.closeHandler(unused -> {
+                                getVertx().cancelTimer(pingTaskId);
                                 if (getVertx().deploymentIDs().contains(deploymentID())) {
                                     register(registerClient, options, vertxHttpGatewayConnectorHandler);
                                 }
